c36de0d55bbc62ff09d0d2c9062ecaab948f3304
Dmitriy Shein <trollde.troll@yandex.ru>
2021-04-24 16:46:19 +0300

    visual improvements

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,15 +5,19 @@ import Post from "./Post/Post"
 const MyPosts = () => {
   return (
     <div className={s.postsArea}>
-      My posts
+      <h3>My posts</h3>
       <div>
-        <textarea />
-        <button>Add Post</button>
+        <div>
+          <textarea/>
+        </div>
+        <div>
+          <button>Add Post</button>
+        </div>
       </div>
       <div className={s.posts}>
-        <Post message='Hi, how are you?)' likes='3,8k' />
-        <Post message='Fine :)' likes='1,6k' />
-        <Post message='wtf are u doing?' likes='255' />
+        <Post message='Hi, how are you?)' likes='3,8k'/>
+        <Post message='Fine :)' likes='1,6k'/>
+        <Post message='wtf are u doing?' likes='255'/>
       </div>
     </div>
   );
