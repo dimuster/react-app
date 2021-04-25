a44235496ffe251530b9ba8391f652633fcd99be
Dmitriy Shein <trollde.troll@yandex.ru>
2021-04-26 01:22:40 +0300

    components moved to file

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
 import s from './Dialogs.module.css';
-
-const DialogItem = (props) => {
-  const path = '/dialogs/' + props.id;
-  return (
-    <div className={s.dialog}>
-      <NavLink to={path} activeClassName={s.active}>{props.name}</NavLink>
-    </div>
-  );
-};
-
-const Message = (props) => {
-  return (
-    <div className={s.message}>{props.text}</div>
-  );
-}
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 const Dialogs = () => {
   const dialogs = [
