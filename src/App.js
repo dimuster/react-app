b5392a2a58f5443099eb6800f01ab25e60e424d1
Dmitriy Shein <trollde.troll@yandex.ru>
2021-04-28 00:11:03 +0300

    optimized <Route/>

--- src/App.js
+++ src/App.js
@@ -17,9 +17,7 @@ const App = (props) => {
         <Header/>
         <Navbar/>
         <div className="app-wrapper-content">
-          <Route path={'/dialogs'} render={ () => {
-            return <Dialogs dialogs={props.dialogs} messages={props.messages} />;
-          }} />
+          <Route path={'/dialogs'} render={ () => <Dialogs dialogs={props.dialogs} messages={props.messages} />}/>
           <Route path={'/profile'} render={ () => <Profile posts={props.posts}/>} />
           <Route path={'/news'} render={ () => <News/>} />
           <Route path={'/music'} render={ () => <Music/>} />
