06bf56ed8ee53b8208fc71ac68aaf3dd8a540a0d
Dmitriy Shein <trollde.troll@yandex.ru>
2021-04-29 20:52:03 +0300

    created simple logic in Virtual DOM

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,16 +3,20 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
+  const addPostElement = React.createRef();
+  const addPost = () => {
+    alert(addPostElement.current.value);
+  }
   const postsElements = props.posts.map(p => <Post text={p.text} likes={p.likes}/>);
   return (
     <div className={s.postsArea}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea/>
+          <textarea ref={addPostElement}/>
         </div>
         <div>
-          <button>Add Post</button>
+          <button onClick={addPost}>Add Post</button>
         </div>
       </div>
       <div className={s.posts}>
